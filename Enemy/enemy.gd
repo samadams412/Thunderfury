b46acc975836f1e6e64f5fb06fe1880f21a55bd6
Samuel Adams <Samueladams412@gmail.com>
2026-07-04 01:37:13 -0500

    Updated spritesheets, added animation player for enemy_goblin and enemy_ragnaros

---
+++ Enemy/enemy.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var movement_speed = 20.0
+# Add this: Set this to true for Ragnaros, false for the Goblin in the Inspector
+@export var use_directional_animations : bool = false 
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite = $Sprite2D
+@onready var anim = $AnimationPlayer
+
+func _ready():
+	if use_directional_animations:
+		anim.play("walk_left") # Start with a default
+	else:
+		anim.play("walk")
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
+	
+	if use_directional_animations:
+		# Ragnaros Logic
+		if direction.x < 0:
+			if anim.current_animation != "walk_left":
+				anim.play("walk_left")
+		else:
+			if anim.current_animation != "walk_right":
+				anim.play("walk_right")
+	else:
+		# Goblin Logic
+		if direction.x > 0.1:
+			sprite.flip_h = true
+		elif direction.x < -0.1:
+			sprite.flip_h = false
--- Player/player.gd
+++ Player/player.gd
@@ -1,12 +1,29 @@
 extends CharacterBody2D
 
 var movement_speed = 60.0
+@onready var sprite = $Sprite2D
+@onready var walkTimer = get_node("%walkTimer")
 
-func _physics_process(_delta): # Use _delta to show it is unused
+func _physics_process(_delta):
 	movement()
 	
 func movement():
-	# get_vector automatically handles the subtraction logic for you
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * movement_speed
+	# Optimized vector movement
+	var mov = Input.get_vector("left", "right", "up", "down")
+	
+	# Handle Sprite Flipping
+	if mov.x > 0:
+		sprite.flip_h = true
+	elif mov.x < 0:
+		sprite.flip_h = false
+		
+	# Animation Logic (Tutorial style)
+	if mov != Vector2.ZERO:
+		if walkTimer.is_stopped():
+			# This logic cycles through your frames
+			sprite.frame = 0 if sprite.frame >= sprite.hframes - 1 else sprite.frame + 1
+			walkTimer.start()
+	
+	# Physics
+	velocity = mov * movement_speed
 	move_and_slide()
